b6466a4fe660f8dc74f2cf423578024f116517ad
pelayolartategui <pelayolartategui@gmail.com>
2017-03-27 15:47:24 +0200

    Configuración

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,11 +3,16 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import participationSystem.gestionSugerencias.Services;
+import participationSystem.model.Configuration;
+
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
     	SpringApplication.run(Application.class, args);
+        Configuration config = Services.getSystemServices().getConfiguration();
+
     }
 }
