2fdb9f58f6d0f74b33aac2ab77918bd4d1c21238
anabravo25 <anabravo25@hotmail.com>
2017-04-01 16:24:26 +0200

    A ver si pasa selenium

--- src/test/java/participationSystem/SeleniumTest.java
+++ src/test/java/participationSystem/SeleniumTest.java
@@ -9,17 +9,23 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.test.context.web.WebAppConfiguration;
 import participationSystem.util.SeleniumUtils;
+import cucumber.api.java.es.Cuando;
+import cucumber.api.java.es.Entonces;
 
+@SuppressWarnings("deprecation")
+@IntegrationTest
 @WebAppConfiguration
 public class SeleniumTest {
 	private WebDriver driver;
 	private String baseUrl;
 	private StringBuffer verificationErrors = new StringBuffer();
 	
+	@Cuando("^inicia sesion$")
 	@Before
-	public void setUp() throws Exception {
+	public void inicia_sesion() throws Exception {
 		driver = new HtmlUnitDriver();
 		baseUrl = "http://localhost:8080/";
 		driver.get(baseUrl);
@@ -44,6 +50,7 @@ public class SeleniumTest {
 	}
 	
 	@Test
+	@Entonces("^vota positivo$")
 	public void testVotarPositivo() throws Exception {
 		//Selenium.waitForPageToLoad("30000");
 		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[1]/td[1]")).getText(), "Marquesina Llamaquique");
@@ -66,7 +73,7 @@ public class SeleniumTest {
 	
 	
 	
-	@Test
+	@Entonces("^vota negativo$")
 	public void testVotarNegativo() throws Exception {
 		driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[2]/td[5]/form/button")).click();
 		SeleniumUtils.EsperaCargaPaginaxpath(driver, "//*[@id=\"sugerencias\"]/tbody/tr[2]/td[3]", 3);
@@ -79,7 +86,7 @@ public class SeleniumTest {
 		*/
 	}
 	
-	@Test
+	@Entonces("^Busca la sugerencia$")
 	public void testBuscarSugerencia() throws Exception {
 		/*driver.findElement(By.xpath("//*[@id=\"sugerencias_filter\"]/label/input")).sendKeys("HUCA");
 		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr/td[1]")).getText(), "Lineas de bus Nuevo HUCA");
@@ -91,8 +98,8 @@ public class SeleniumTest {
 		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr/td[3]")), 1);		
 		*/
 	}
-	
 	@Test
+	@Entonces("^ordena las sugerencias por categoria$")
 	public void testPorCategoria() throws Exception {
 		driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/thead/tr/th[2]")).click();
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
@@ -113,22 +120,24 @@ public class SeleniumTest {
 	}
 	
 	@Test
+	@Entonces("^ordena las sugerencias por titulo$")
 	public void testOrdenarSugerenciasPorTitulo() throws Exception {		
 		driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/thead/tr/th[1]")).click();
 		for(int i=1;i<4;i++)
 			System.out.println(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr["+i+"]/td[1]")).getText());
 		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[1]/td[1]")).getText(), "Marquesina Llamaquique");
-		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[2]/td[1]")).getText(), "Nuevos Ã¡rboles");
+		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[2]/td[1]")).getText(), "Nuevos árboles");
 	
 		/*assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[1]/td[1]")).getText(), "Activar ayudas a la eficiencia energética para apoyar la rehabilitación");
 		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[2]/td[1]")).getText(), "Ampliacion de horario de recogida");
 	*/}
 	
 	@Test
+	@Entonces("^ordena las sugerencias por contenido$")
 	public void testOrdenarSugerenciasPorContenido() throws Exception {		
 		driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/thead/tr/th[1]")).click();
 		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[1]/td[1]")).getText(), "Marquesina Llamaquique");
-		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[2]/td[1]")).getText(), "Nuevos Ã¡rboles");
+		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[2]/td[1]")).getText(), "Nuevos árboles");
 		
 		/*assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[1]/td[1]")).getText(), "Ampliar el horario de la zona verde e incluir los domingos");
 		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[2]/td[1]")).getText(), "Activar ayudas a la eficiencia energética para apoyar la rehabilitación");
@@ -136,16 +145,18 @@ public class SeleniumTest {
 	}
 	
 	@Test
+	@Entonces("^ordena las sugerencias por votos$")
 	public void testOrdenarSugerenciasPorVotos() throws Exception {		
 		driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/thead/tr/th[3]")).click();
 		//assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[1]/td[1]")).getText(), "Ampliacion de horario de recogida");
 		//assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[2]/td[1]")).getText(), "Ampliar horario");
 		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[1]/td[1]")).getText(), "Marquesina Llamaquique");
-		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[2]/td[1]")).getText(), "Nuevos Ã¡rboles");
+		assertEquals(driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[2]/td[1]")).getText(), "Nuevos árboles");
 
 	}
 	
 	@Test
+	@Entonces("^muestra los comentarios$")
 	public void testMostrarComentarios() throws Exception {		
 		driver.findElement(By.xpath("//*[@id=\"sugerencias\"]/tbody/tr[2]/td[1]/a")).click();
 		assertEquals(driver.findElement(By.xpath("/html/body/div/div/div[2]/"
